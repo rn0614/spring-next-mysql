5d3e24c5881b37066cf10d5213913bc615aa7a19
rn0614 <rn0614@naver.com>
2024-07-06 19:47:24 +0900

    메모리 문제로 threejs 모듈 관련 미배포

--- front/src/pages/interactive/index.tsx
+++ front/src/pages/interactive/index.tsx
@@ -1,37 +1,37 @@
-import { Canvas } from "@react-three/fiber";
-import { useControls } from "leva";
-import CameraThreeElement from "../../components/three/CameraThreeElement";
-import styles from "./style.module.scss";
+// import { Canvas } from "@react-three/fiber";
+// import { useControls } from "leva";
+// import CameraThreeElement from "../../components/three/CameraThreeElement";
+// import styles from "./style.module.scss";
 
-export default function InteractivePage() {
-  const { color, segment } = useControls({
-    color: "green",
-    segment: { value: 10, min: 2, max: 100, step: 1 },
-  });
+// export default function InteractivePage() {
+//   const { color, segment } = useControls({
+//     color: "green",
+//     segment: { value: 10, min: 2, max: 100, step: 1 },
+//   });
 
-  return (
-    <div className={styles.wrapper}>
-      <Canvas
-        shadows
-        //orthographic  // 기본이 perspective
-        camera={{
-          //zoom:150, // orthographic 의 거리
-          near: 1,
-          far: 20,
-          fov: 75, // 카메라 각도
-          position: [5, 5, 5],
-        }}
-      >
-        {/* <color attach="background" args={["black"]} /> */}
-        {/* <fog attach={"fog"} args={["white", 3, 10]} /> */}
+//   return (
+//     <div className={styles.wrapper}>
+//       <Canvas
+//         shadows
+//         //orthographic  // 기본이 perspective
+//         camera={{
+//           //zoom:150, // orthographic 의 거리
+//           near: 1,
+//           far: 20,
+//           fov: 75, // 카메라 각도
+//           position: [5, 5, 5],
+//         }}
+//       >
+//         {/* <color attach="background" args={["black"]} /> */}
+//         {/* <fog attach={"fog"} args={["white", 3, 10]} /> */}
 
-        <axesHelper args={[5]} />
-        <gridHelper args={[20, segment, 0xff0000, "teal"]} />
-        {/* <ThreeElement/> */}
-        {/* <InteractiveThreeElement/> */}
-        {/* <InteractCar color={"red"}/> */}
-        <CameraThreeElement />
-      </Canvas>
-    </div>
-  );
-}
+//         <axesHelper args={[5]} />
+//         <gridHelper args={[20, segment, 0xff0000, "teal"]} />
+//         {/* <ThreeElement/> */}
+//         {/* <InteractiveThreeElement/> */}
+//         {/* <InteractCar color={"red"}/> */}
+//         <CameraThreeElement />
+//       </Canvas>
+//     </div>
+//   );
+// }
